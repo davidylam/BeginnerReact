f24f09ab9f5e507047340d9667adba1def1eb50b
David Lam <DavidYLam@yahoo.com>
2018-02-27 08:28:43 -1000

    Convert Header component to Stateless Function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
+
+const Header = (props) => {
     return (
       <header className="top">
       	<h1>
@@ -13,11 +13,9 @@ class Header extends React.Component {
       		Day
       	</h1>
       	<h3 className="tagline">
-      		<span>{this.props.tagline}</span>
+      		<span>{props.tagline}</span>
       	</h3>
       </header>
     )
-  }
 }
-
 export default Header;
