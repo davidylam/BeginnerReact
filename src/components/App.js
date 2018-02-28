b00733d35c1c902ce4a36e750ada64154ef4828c
David Lam <DavidYLam@yahoo.com>
2018-02-27 15:25:58 -1000

    Set State, and pass to component

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,32 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.addFish = this.addFish.bind(this)
+    this.loadSamples = this.loadSamples.bind(this)
+    this.state = {
+      fishes:{},
+      order: {}
+    };
+  }
+
+  addFish(fish){
+    const fishes = {...this.state.fishes};
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    this.setState({fishes});
+  }
+
+  loadSamples(){
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +35,7 @@ class App extends React.Component {
           <Header age="25" tagline="Best Fish Yet" fullName={true} />
         </div>
         <Order />
-        <Inventory/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -10,16 +10,24 @@ class StorePicker extends React.Component {
   goToStore(event) {
     event.preventDefault();
     console.log("yyy: " + this.storeInput.value);
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
   render() {
     return (
       <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input)=>{this.storeInput = input}}/>
+        <input type="text" required placeholder="Store Name" 
+          defaultValue={getFunName()} ref={(input)=>{this.storeInput = input}}
+        />
         <button type="submit">Visit Store →</button>
       </form>
     )
   }
 }
 
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
 export default StorePicker;
